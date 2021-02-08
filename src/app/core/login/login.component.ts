8ba1ac13618bf596e9fed8ccdaf8f0ca8903c6e6
Marko Radulovic <markoradulovic@twognation.com>
2021-02-08 12:08:40 +0100

    form changes

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService, LoginRequest, LoginResponse } from '../auth/auth.service';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   templateUrl: './login.component.html',
@@ -8,6 +9,7 @@ import { AuthService, LoginRequest, LoginResponse } from '../auth/auth.service';
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
+  errorMessage = '';
 
   constructor(private fb: FormBuilder, private auth: AuthService) {}
 
@@ -17,18 +19,31 @@ export class LoginComponent implements OnInit {
 
   createForm(): void {
     this.loginForm = this.fb.group({
-      username: ['', Validators.required],
+      username: [
+        '',
+        Validators.compose([Validators.required, Validators.email]),
+      ],
       password: ['', Validators.required],
       rememberMe: [false, Validators.requiredTrue],
+    });
+
+    this.loginForm.valueChanges.pipe(debounceTime(500)).subscribe((value) => {
+      console.log(value);
+      this.errorMessage = '';
     });
   }
 
   submitForm(value: LoginRequest): void {
-    this.auth
-      .login(value)
-      .subscribe((response: LoginResponse) =>
-        localStorage.setItem('token', response.id_token)
-      );
+    this.auth.login(value).subscribe(
+      (response: LoginResponse) => {
+        localStorage.setItem('token', response.id_token);
+        this.errorMessage = '';
+      },
+      (error) => {
+        console.log(error);
+        this.errorMessage = error.error.detail;
+      }
+    );
     this.auth.getData().subscribe((result) => console.log(result));
   }
 }
