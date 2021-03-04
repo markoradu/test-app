967f3a77ae25c83d54988f5f28a037c7d2beed04
Marko Radulovic <markoradulovic@twognation.com>
2021-03-04 10:50:48 +0100

    search, clear and game filters

--- src/app/core/tournaments/tournaments-filters/tournaments-filters.component.ts
+++ src/app/core/tournaments/tournaments-filters/tournaments-filters.component.ts
@@ -1,8 +1,9 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { faMinus, faPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
+import { TournamentService } from '../tournaments.service';
 
 @Component({
   selector: 'app-tournaments-filters',
@@ -12,11 +13,22 @@ import { debounceTime } from 'rxjs/operators';
 export class TournamentsFiltersComponent implements OnInit {
   tournamentFinderForm!: FormGroup;
   faSearch = faSearch;
+  faMinus = faMinus;
+  faPlus = faPlus;
   searchByName$: Subject<any> = new Subject<string>();
   @Output() emitForm = new EventEmitter();
   @Output() resetFilters = new EventEmitter();
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private tournamentService: TournamentService
+  ) {}
+
+  get gameId_in(): FormArray | null {
+    return this.tournamentFinderForm
+      ? (this.tournamentFinderForm.controls['gameId.in'] as FormArray)
+      : null;
+  }
 
   ngOnInit(): void {
     this.searchByName$.pipe(debounceTime(400)).subscribe((name) => {
@@ -25,19 +37,92 @@ export class TournamentsFiltersComponent implements OnInit {
     this.initForm();
   }
 
-  initForm(): void {
-    this.tournamentFinderForm = this.fb.group({
-      'name.contains': [''],
+  initForm(game: any = []): void {
+    this.tournamentService.getGames().subscribe((result) => {
+      game = result;
+      this.tournamentFinderForm = this.fb.group({
+        'gameId.in': this.populateForm(game),
+        'name.contains': [''],
+      });
     });
+  }
+
+  populateForm(game: any, name?: any): any {
+    const arr: Array<any> = [];
+    const items = game;
+
+    items.forEach((element: object) => {
+      arr.push(this.newFormItem(element, game, name));
+    });
+
+    return this.fb.array(arr);
+  }
+
+  newFormItem(item: any, game?: any, name?: string): FormGroup {
+    let isChecked: boolean = false;
+
+    if (game && item.id === game.id) {
+      isChecked = true;
+    }
+
+    return this.fb.group({
+      id: item.id,
+      name: item.name,
+      checked: isChecked,
+      count: item.tournamentCount ? item.tournamentCount : null,
+    });
+  }
+  removeEmpty(currentObject: Array<any>): object {
+    const result: any = {};
+
+    for (const key in currentObject) {
+      if (currentObject.hasOwnProperty(key)) {
+        if (
+          currentObject[key] &&
+          currentObject[key].length &&
+          currentObject[key] !== 'default'
+        ) {
+          typeof currentObject[key] !== 'string'
+            ? (result[key] = currentObject[key].join(','))
+            : (result[key] = currentObject[key]);
+        }
+      }
+    }
+
+    return result;
+  }
+
+  extractChecked(formObj: any) {
+    const tmpForm: any = {};
+
+    for (const key in formObj) {
+      if (formObj.hasOwnProperty(key) && formObj[key] != null) {
+        const element = formObj[key];
+        if (typeof element !== 'string' && typeof element !== 'number') {
+          tmpForm[key] = [];
+
+          element.map((elem: any) => {
+            if (elem.checked) {
+              tmpForm[key].push(elem.id);
+            }
+          });
+        } else {
+          tmpForm[key] = element.toString();
+        }
+      }
+    }
+
+    return tmpForm;
   }
 
   submitForm(form: any): void {
     let { value } = form;
-    this.emitForm.next(value);
+    value = this.extractChecked(value);
+    this.emitForm.next(this.removeEmpty(value));
   }
 
   resetForm(): void {
     this.initForm();
-    this.resetFilters.emit();
+    this.resetFilters.emit(true);
   }
 }
--- src/app/core/tournaments/tournaments.service.ts
+++ src/app/core/tournaments/tournaments.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,11 @@ export class TournamentService {
 
   getTournaments(filter: any): Observable<any> {
     const uri = 'tournaments';
-
     return this.http.get<any>(uri, { params: filter, observe: 'response' });
+  }
+
+  getGames(): Observable<any> {
+    const uri = 'get-games-by-external-site/' + environment.externalSite;
+    return this.http.get(uri);
   }
 }
