ca2e8c2a2e7d11e7ac580dd8749af4184d5f3500
Marko Radulovic <markoradulovic@twognation.com>
2021-03-22 16:25:30 +0100

    content creator form skeleton

--- src/app/core/content-cretor/content-cretor.component.ts
+++ src/app/core/content-cretor/content-cretor.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-content-cretor',
   templateUrl: './content-cretor.component.html',
-  styleUrls: ['./content-cretor.component.scss']
+  styleUrls: ['./content-cretor.component.scss'],
 })
 export class ContentCretorComponent implements OnInit {
+  creatorForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  createForm(): void {
+    this.creatorForm = this.fb.group({
+      firstName: [],
+      lastName: [],
+      phoneNumber: [],
+      day: [],
+      month: [],
+      year: [],
+      language: [],
+      streamPlatform: [],
+      streamName: [],
+      message: [],
+    })
+  }
 }
